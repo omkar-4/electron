36f776ae9575ea16a51908386501e64d5ff89ef5
omkar-4 <omkar.pro04@gmail.com>
2024-09-13 02:34:12 +0530

    second commit

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
-const path = require('node:path')
+const { app, BrowserWindow, ipcMain } = require("electron/main");
+const path = require("node:path");
+const { updateElectronApp } = require("update-electron-app");
+updateElectronApp({ forceDev: true });
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-  win.loadFile('index.html')
-}
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  win.loadFile("index.html");
+};
 app.whenReady().then(() => {
-  ipcMain.handle('ping', () => 'pong')
-  createWindow()
-})
+  ipcMain.handle("ping", () => "pong");
+  createWindow();
+});
